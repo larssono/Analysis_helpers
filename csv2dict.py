28bd9cbcd9059f8bc1d04ccf08288809393f0aac
Larsson Omberg <lom@larssono.com>
2010-11-10 15:33:24 -0500

    Changed csv2dict to handle other separators than \t

--- csv2dict.py
+++ csv2dict.py
@@ -1,14 +1,25 @@
-import numpy as np
+import gzip, numpy as np
+
+def openfile(file):
+    """Returns a file object by opening either a gz or uncompressed file. """
+    if file.endswith('.gz'):
+        return gzip.open(file)
+    else:
+        return open(file)
 
 
-def csv2Dict(file):
+def csv2Dict(file, sep='\t'):
     """Given a tab delimited file returns the first column as list and
-    remaining columns as dictionary of lists."""    
-    fp=open(file)
-    head=fp.readline().rstrip().split('\t')
+    remaining columns as dictionary of lists.
+    Parameters:
+    `file` input file
+    `sep` - optional parameter incase file is not delimited by tab
+    """    
+    fp=openfile(file)
+    head=fp.readline().rstrip().split(sep)
     head=[l.strip('"') for l in head]
     head= dict(zip(head[1:], range(1, len(head)))) #translation between names and columns
-    vals=[line.split('\t') for line in fp] 
+    vals=[line.split(sep) for line in fp] 
     phenotypes={}
     subjects=[val[0].strip('"') for val in vals] 
     for key, i in head.items(): 
@@ -26,7 +37,7 @@ def csv2DictDict(file, sep='\t'):
           fileDict=csv2DictDict(file)
           fileDict['NML-001']['age']  -> 24
           fileDict['NML-002']['gender'] -> F"""    
-    fp=open(file)
+    fp=openfile(file)
     head=fp.readline().rstrip().split(sep)
     head=[l.strip('"') for l in head]
     head= dict(zip(head[1:], range(1, len(head)))) #translation between names and columns
