6b3554504e0ad0f87f0c7c525fd03c6b635890c0
Larsson Omberg <lom@larssono.com>
2010-11-15 15:54:16 -0500

    Changed fst to take two matrices and added fstFile that does what original fst does.  Also added parameter isNorm to either to size normalized (as for HapMap) or naive calculation of fst.

--- popgen.py
+++ popgen.py
@@ -6,30 +6,32 @@ import fileReader
 def nchoose2(n):
     return n*(n-1)/2
 
-def fst(file1, file2):
+def fst(vals0, vals1, isNorm=True):
+    nij=np.asarray([vals0.shape[1], vals1.shape[1]], np.float)
+    nAll=nij.sum()
+    xi=((vals0==-1).sum(1)+(vals1==-1).sum(1))/nAll
+    xij=[(vals0==-1).sum(1)/nij[0], (vals1==-1).sum(1)/nij[1]]
+    if isNorm:
+        top=0; bottom=0
+        for j in range(2):
+            top+=nchoose2(nij[j])*np.sum(2*nij[j]/(nij[j]-1)*xij[j]*(1-xij[j]))
+            bottom+=nchoose2(nij[j])
+        return 1-top/bottom/np.sum(2*nAll/(nAll-1)*xi*(1-xi))
+    return 1-(np.sum((1-xij[0])*xij[0])+np.sum((1-xij[1])*xij[1]))/2/ np.sum((1-xi)*xi)
+
+def fstFile(file1, file2, isNorm=True):
+    """Calculates fst based on data stored in two tab delimited files.
+    Each file must contain the following columns: snp names; snp position; snp values. """
+
     files=fileReader.concurrentFileReader(file1, file2, key=1)
     subjects=files.next()
     vals=[]
     for (snpName, snpLocation, snps) in files:
         vals.append(fileReader.nucleotides2Haplotypes(sum(snps, [])))
-    vals=np.asarray(vals)
-    nAll=float(vals.shape[1])
-    nij=np.asarray(map(len, subjects), np.float)
-    popIdx=[range(nij[0]), range(nij[0],vals.shape[1])] 
+    vals=np.asarray(vals, np.float)
+    nSamples=len(subjects[0])
+    return fst(vals[:,:nSamples], vals[:,nSamples:], isNorm)
 
-    xi=((vals==-1).sum(1)/nAll)
-    xij=[]
-
-    for i, n in enumerate(nij):
-        idx=popIdx[i]
-        xij.append((vals[:,idx]==-1).sum(1)/n)
-
-    top=0; bottom=0
-    for j in range(2):
-        top+=nchoose2(nij[j])*np.sum(2*nij[j]/(nij[j]-1)*xij[j]*(1-xij[j]))
-        bottom+=nchoose2(nij[j])
-
-    return 1-top/bottom/np.sum(2*nAll/(nAll-1)*xi*(1-xi)),  1-(sum((1-xij[0])*xij[0])+sum((1-xij[1])*xij[1]))/2/ sum((1-xi)*xi)
 
 if __name__ == '__main__':
-    print fst(*sys.argv[1:])
+    print fstFile(*sys.argv[1:])
